2d7716eca824f5d2f6dcb5b6cc8fc6fcb1e0a7fc
garrettbadger <garrett.badger.id@gmail.com>
2021-11-18 14:50:18 -0700

    raylib

--- 08-rfk/rfk/__main__.py
+++ 08-rfk/rfk/__main__.py
@@ -1,3 +1,6 @@
+import os
+os.environ['RAYLIB_BIN_PATH'] = r'C:\Users\flipp\cse-210-solo-checkpoints\cse210-student-solo-checkpoints\07-snake\raylib-2.0.0-Win64-mingw\raylib-2.0.0-Win64-mingw\lib'
+
 import random
 from game import constants
 from game.director import Director
--- 08-rfk/rfk/game/draw_actors_action.py
+++ 08-rfk/rfk/game/draw_actors_action.py
@@ -1,3 +1,15 @@
+from ctypes import set_errno
 from game.action import Action
 
 # TODO: Add your DrawActorsAction class here
+class DrawActorsAction(Action):
+    def __init__(self, output_service) -> None:
+        super().__init__()
+
+        self._output_service = output_service
+    
+    def execute(self, cast):
+        self._output_service.clear_screen()
+        for group in cast.values():
+            self._output_service.draw_actors(group)
+        self._output_service.flush_buffer()
